173b1ae46a7eec5ab41b216611a4a0cd80141c7f
AlonsoIndex18 <alonso_avalo97@hotmail.com>
2021-10-29 17:43:02 -0500

    added threejs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,64 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 import './App.css';
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+
+//Camera
+const camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight,0.6,1200);
+camera.position.z = 5;
+
+//Loader
+const loader = new FBXLoader();
+
+
+//Renderer
+const renderer = new THREE.WebGLRenderer({antialias: true});
+renderer.setClearColor("#223143");
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+//Canvas responsive
+window.addEventListener('resize', () => {
+    renderer.setSize(window.innerWidth,window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+})
+
+//Create object
+const boxGeometry = new THREE.BoxGeometry(2,2,2);
+const boxMaterial = new THREE.MeshLambertMaterial({color: 0xFFFFFF});
+const boxMesh = new THREE.Mesh(boxGeometry,boxMaterial);
+boxMesh.rotation.set(40,0,40);
+scene.add(boxMesh);
+
+//Load a model
+loader.load("models/fbx/akiko-sama-anime-character-model/source/LOD0.fbx", function(object){
+    scene.add(object.scene);
+}, undefined, function (error) {
+    console.error(error);
+
+});
+
+
+//Light
+const light = new THREE.PointLight(0xFFFFFF, 1, 100);
+light.position.set(5,5,5);
+scene.add(light);
+
+
+const rendering = function() {
+    requestAnimationFrame(rendering);
+
+    scene.rotation.z -=0.05;
+    scene.rotation.x -=0.01;
+    renderer.render(scene,camera);
+}
+rendering();
+
+
 
 function App() {
   const [date, setDate] = useState(null);
@@ -14,6 +72,7 @@ function App() {
   }, []);
   return (
     <main>
+      <scene/>
       <h1>Create React App + Go API</h1>
       <h2>
         Deployed with{' '}
